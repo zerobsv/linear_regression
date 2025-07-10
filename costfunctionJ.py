b111e27d6769ce345c0ddb15e1307f30c0894d17
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2025-07-10 21:31:51 +0300

    Correcting  the diemensions of the matrices

--- costfunctionJ.py
+++ costfunctionJ.py
@@ -4,7 +4,7 @@ import random
 # Calculate the cost function J for linear regression
 def costFunctionJ(X,y,theta):
 	m 			= X.shape[0]
-	prediction 	= X * theta
+	prediction 	= np.dot(X, theta) # or np.matmul(X, theta)
 	print("Prediction: ", prediction, prediction.shape)
 	print("Y: ", y, y.shape)
 	Error  		= np.subtract(prediction, y)
@@ -14,14 +14,12 @@ def costFunctionJ(X,y,theta):
 
 # Perform gradient descent to minimize the cost function
 def gradientDescent(X,y,theta,alpha,m,numIter):
-	xTrans      = np.transpose(X)
+	Xt = X.T
 	for x in range(numIter):
-		prediction 	= np.dot(xTrans, theta)
+		prediction 	= np.dot(X, theta)
 		Error  		= np.subtract(prediction, y)
 		print("Error: ", Error, Error.shape)
-		print("Theta: ", theta, theta.shape)
-		print("X Transpose: ", xTrans, xTrans.shape)
-		gradient = np.dot(X, Error)
+		gradient = np.dot(Xt, Error)
 		gradient = gradient * (1.0/m)
 		print("Gradient: " , gradient, gradient.shape)
 		theta = theta - (alpha * gradient)
@@ -29,30 +27,35 @@ def gradientDescent(X,y,theta,alpha,m,numIter):
 	return theta
 
 # Calculate the minimal cost function using the normal equation
-def minimal_cost_function():
-	xTrans = np.transpose(X)
-	invTerm = np.dot(xTrans, X)
+def minimal_cost_function(X, y):
+	product = X.T @ X # or np.dot
+	print("Product: ", product, product.shape)
+	invTerm = np.linalg.inv(product)
 	print("Inverse Term: ", invTerm, invTerm.shape)
-	invTerm = np.linalg.inv(invTerm)
-	return np.dot(np.dot(invTerm, xTrans), y)
+	secondTerm = X.T @ y # or np.dot
+	print("Second Term: ", secondTerm, secondTerm.shape)
+	theta = np.dot(invTerm, secondTerm)
+	print("Inverse Term X Transpose: ", secondTerm, secondTerm.shape)
+	# theta = inv(X^T * X) * (X^T * y)
+	# where X is the design matrix and y is the target variable
+	return theta
 
 def genData(numPoints, bias, variance):
-	x = np.zeros(shape=(2, numPoints))
-	y = np.zeros(shape=numPoints)
+	x = np.ones(shape=(numPoints, 2))
+	y = np.ones(shape=numPoints)
 	# basically a straight line
 	for i in range(0, numPoints):
-		# bias feature 
-		x[0][i] = 1
-		x[1][i] = i
+		x[i][1] = i
 		# our target variable
 		y[i] = (i + bias) + random.uniform(0, 1) * variance
 	return x, y
 
 if __name__ == '__main__':
 	X , y = genData(100,2.5,0.1)
-	theta = np.ones((2, 100))
+	theta = np.ones((2, 100)) # Shape (2, 100) for two features (bias and x)
 	numIter = 10
 	alpha = 0.005
+
 
 	initialcost = costFunctionJ(X,y,theta)
 
@@ -64,6 +67,11 @@ if __name__ == '__main__':
 	print("Initial Cost: ", initialcost)
 	print("Final Cost: ", finalcost)
 
-	# # Using the normal equation
-	# theta = minimal_cost_function()
-	# print(theta)
+	# Using the normal equation
+	theta = minimal_cost_function(X, y)
+	print(theta)
+	finalcost = costFunctionJ(X,y,theta)
+	print("Final Cost Using Normal Equation: ", finalcost)
+
+
+
